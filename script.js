8aaa39337fa668395f44f1d662b11ef2f555bab2
ksanoon <nooncostumercare@gmail.com>
2025-06-02 21:36:50 +0300

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const orders = [
   { trackingNumber: "1772", name: "Muhibbulla", phone: "+966*******", delivery: "400 <span style='font-family: Arial;'>&#65020;</span>", date: "1 May" },
   { trackingNumber: "1773", name: "Hasnain Quraishi", phone: "+966*******", delivery: "400 <span style='font-family: Arial;'>&#65020;</span>", date: "4 June" },
   // ... baki data yahan paste karen ...
-[
+
   { trackingNumber: "1772", name: "Muhibbulla", phone: "+966*******", delivery: "400 <span style='font-family: Arial;'>&#65020;</span>", date: "1 May" },
   { trackingNumber: "1773", name: "Hasnain Quraishi", phone: "+966*******", delivery: "400 <span style='font-family: Arial;'>&#65020;</span>", date: "4 June" },
   { trackingNumber: "1774", name: "Rajo", phone: "+966*******", delivery: "400 <span style='font-family: Arial;'>&#65020;</span>", date: "2 June" },
